21af45e4dcdbb3156561df1b294318c591f14f1f
SilviaZlo <silvianevesmayer@gmail.com>
2023-05-30 15:11:36 -0400

    added Header.js and Footer.js files, as well as an assets folder

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+
+    console.log('Footer is rendering.');
+
+    return (
+        <footer>
+            <div class="wrapper">
+                <a href="https://www.junocollege.com" target="_blank"><p>Created at Juno College</p></a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+
+    console.log('Header is rendering.');
+
+    return (
+        <header>
+            <div class="wrapper">
+                <h1><span class="headerSpan">Choose your preferred cuisine:</span></h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
